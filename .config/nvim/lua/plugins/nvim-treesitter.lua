b0f2bb8fe152a83727f49f939cf69f8c8365bd64
yu-hasebe <yuhasebe0821@gmail.com>
2025-10-12 20:09:01 +0900

    feat: treesitter bash

--- .config/nvim/lua/plugins/nvim-treesitter.lua
+++ .config/nvim/lua/plugins/nvim-treesitter.lua
@@ -4,7 +4,7 @@ return {
 		tag = "v0.10.0",
 		config = function()
 			require("nvim-treesitter.configs").setup({
-				ensure_installed = { "go", "lua" },
+				ensure_installed = { "go", "lua", "bash" },
 				highlight = {
 					enable = true,
 				},
