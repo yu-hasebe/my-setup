928a57d5299b4818562069e95f900a535a3bed56
yu-hasebe <yuhasebe0821@gmail.com>
2025-12-07 21:27:13 +0900

    fix: oil keymaps

--- .config/nvim/lua/plugins/oil.lua
+++ .config/nvim/lua/plugins/oil.lua
@@ -1,6 +1,13 @@
 return {
-  "stevearc/oil.nvim",
-  opts = {},
-  dependencies = { { "nvim-mini/mini.icons", opts = {} } },
-  lazy = false,
+	"stevearc/oil.nvim",
+	opts = {},
+	dependencies = { { "nvim-mini/mini.icons", opts = {} } },
+	keymaps = {
+		["<CR>"] = "actions.select",
+		["<C-s>"] = { "actions.select", opts = { vertical = true } },
+		["<C-h>"] = { "actions.select", opts = { horizontal = true } },
+		["<C-t>"] = { "actions.select", opts = { tab = true } },
+		["g."] = { "actions.toggle_hidden", mode = "n" },
+	},
+	lazy = false,
 }
