105f5c7acd56d36578fcbe60ba7d6473f08ab354
yu-hasebe <yuhasebe0821@gmail.com>
2026-01-17 16:13:34 +0900

    fix: init.lua

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -9,11 +9,24 @@ vim.api.nvim_create_user_command(
 vim.o.encoding = "utf-8"
 vim.o.number = true
 vim.o.relativenumber = true
-vim.o.tabstop = 4
-vim.o.shiftwidth = 4
+
+-- Indent
 vim.o.expandtab = true
+vim.o.shiftround = true
+vim.o.shiftwidth = 2
+vim.o.softtabstop = 2
+vim.o.tabstop = 2
+
+-- Scroll offset
+vim.o.scrolloff = 3
+
+-- Move the cursor to the next (previous) line across the last (first) character
+vim.o.whichwrap = 'b,s,h,l,<,>,[,],~'
+
 -- Share clipboard with OS
 vim.o.clipboard = "unnamedplus"
+
+-- Show cursorline
 vim.o.cursorline = true
 
 vim.cmd("syntax enable")
@@ -34,6 +47,29 @@ vim.api.nvim_set_keymap("i", "(<Enter>", "(<Enter><Enter>)<up><Tab>", { noremap 
 vim.api.nvim_set_keymap("i", "{<Enter>", "{<Enter><Enter>}<up><Tab>", { noremap = true, silent = true })
 vim.api.nvim_set_keymap("i", "[<Enter>", "[<Enter><Enter>]<up><Tab>", { noremap = true, silent = true })
 vim.api.nvim_set_keymap("i", "<<Enter>", "<<Enter><Enter>><up><Tab>", { noremap = true, silent = true })
+
+local augroup = vim.api.nvim_create_augroup('init.lua', {})
+
+local function create_autocmd(event, opts)
+  vim.api.nvim_create_autocmd(event, vim.tbl_extend('force', {
+    group = augroup,
+  }, opts))
+end
+
+-- Type gx to jump this page:
+-- https://vim-jp.org/vim-users-jp/2011/02/20/Hack-202.html
+create_autocmd('BufWritePre', {
+  pattern = '*',
+  callback = function(event)
+    local dir = vim.fs.dirname(event.file)
+    local force = vim.v.cmdbang == 1
+    local isdirectory = vim.fn.isdirectory(dir) == 0
+    if isdirectory and (force or vim.fn.confirm('"' .. dir .. '" does not exist. Create?', "&Yes\n&No") == 1) then
+      vim.fn.mkdir(vim.fn.iconv(dir, vim.opt.encoding:get(), vim.opt.termencoding:get()), 'p')
+    end
+  end,
+  desc = 'Auto mkdir to save files'
+})
 
 local function lsp_keymaps(bufnr)
 	local opts = { buffer = bufnr, silent = true }
