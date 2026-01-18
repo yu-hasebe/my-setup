c5cfddfe846a1924a323a8bedc607fb11d22f2c4
yu-hasebe <yuhasebe0821@gmail.com>
2026-01-18 12:30:38 +0900

    chore: stylua

--- .config/nvim/lua/helpers/lsp.lua
+++ .config/nvim/lua/helpers/lsp.lua
@@ -1,32 +1,32 @@
 local augroup = vim.api.nvim_create_augroup("lsp.lua", {})
 
-vim.api.nvim_create_autocmd('LspAttach', {
-  group = augroup,
-  callback = function (args)
-    local client = assert(vim.lsp.get_client_by_id(args.data.client_id))
+vim.api.nvim_create_autocmd("LspAttach", {
+	group = augroup,
+	callback = function(args)
+		local client = assert(vim.lsp.get_client_by_id(args.data.client_id))
 
-    vim.keymap.set('n', 'grd', function()
-      vim.lsp.buf.definition()
-    end, { buffer = args.buf, desc = 'vim.lsp.buf.definition()'})
+		vim.keymap.set("n", "grd", function()
+			vim.lsp.buf.definition()
+		end, { buffer = args.buf, desc = "vim.lsp.buf.definition()" })
 
-        vim.keymap.set('n', '<space>i', function()
-      vim.lsp.buf.format({ bufnr = args.buf, id = client.id })
-    end, { buffer = args.buf, desc = 'Format buffer' })
-  end
+		vim.keymap.set("n", "<space>i", function()
+			vim.lsp.buf.format({ bufnr = args.buf, id = client.id })
+		end, { buffer = args.buf, desc = "Format buffer" })
+	end,
 })
 
-vim.lsp.config('*', {
-  root_markers = { '.git' },
+vim.lsp.config("*", {
+	root_markers = { ".git" },
 })
 
-local lua_opts = require('helpers.lsp_lua')
-vim.lsp.config('lua', lua_opts)
-vim.lsp.enable('lua')
+local lua_opts = require("helpers.lsp_lua")
+vim.lsp.config("lua", lua_opts)
+vim.lsp.enable("lua")
 
-local sh_opts = require('helpers.lsp_sh')
-vim.lsp.config('sh', sh_opts)
-vim.lsp.enable('sh')
+local sh_opts = require("helpers.lsp_sh")
+vim.lsp.config("sh", sh_opts)
+vim.lsp.enable("sh")
 
 local go_opts = require("helpers.lsp_go")
-vim.lsp.config('go', go_opts)
-vim.lsp.enable('go')
+vim.lsp.config("go", go_opts)
+vim.lsp.enable("go")
